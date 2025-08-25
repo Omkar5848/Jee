3e4ae1999fb15c4694d06f82466d954f3cc8c524
Omkar5848 <omkarjore066@gmail.com>
2025-08-25 17:36:40 +0530

    Fix GitHub Pages build

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  output: 'export',   // ensures static build for GH Pages
-  images: {
-    unoptimized: true
-  },
   output: 'export',
   basePath: process.env.GITHUB_ACTIONS ? '/Jee' : '',
-  images: { unoptimized: true },
-  basePath: process.env.GITHUB_ACTIONS ? '/Jee' : '',
-};
+  images: { unoptimized: true }
+}
+module.exports = nextConfig
 
-
-module.exports = nextConfig;
