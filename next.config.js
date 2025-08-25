ea416b1354ed8c7a256c163f1b7572f53aa582be
Omkar5848 <omkarjore066@gmail.com>
2025-08-25 17:22:06 +0530

    Initial commit with workflows and config

--- next.config.js
+++ next.config.js
@@ -5,7 +5,11 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
+  output: 'export',
+  basePath: process.env.GITHUB_ACTIONS ? '/Jee' : '',
+  images: { unoptimized: true },
   basePath: process.env.GITHUB_ACTIONS ? '/Jee' : '',
 };
+
 
 module.exports = nextConfig;
